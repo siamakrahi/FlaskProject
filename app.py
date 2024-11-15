1410ea8cb4e762399a31271935a16e10220f6e54
siamak rahi <siamak.rahi@gmail.com>
2024-11-15 20:50:32 +0330

    second commit

--- app.py
+++ app.py
@@ -1,90 +1,103 @@
-from flask import Flask, render_template
-from models import *
+from flask import Flask, request, redirect, url_for, render_template, session, flash
+import sqlalchemy as sa
+from sqlalchemy.orm import declarative_base, sessionmaker
+from sqlalchemy import Enum
+
+Base = declarative_base()
+
+class User(Base):
+    __tablename__ = "User"
+    
+    id = sa.Column(sa.Integer, primary_key=True)
+    first_name = sa.Column(sa.String(255), nullable=False)
+    last_name = sa.Column(sa.String(255), nullable=False)
+    email = sa.Column(sa.String(255), nullable=False)
+    user_request = sa.Column(Enum("Messaging", "Consulting", "Newsletter"))
+    avatar = sa.Column(sa.String(255), nullable=True)
+    about = sa.Column(sa.Text, nullable=True)
+
+engine = sa.create_engine("sqlite:///myproject.db")
+Base.metadata.create_all(engine)
+Sessionmaker = sessionmaker(bind=engine)
+db_session = Sessionmaker()
 
 app = Flask(__name__)
+app.secret_key = 'your_secret_key'
 
+@app.route('/register', methods=['POST', 'GET'])
+def create_user():
+    if request.method == "POST":
+        fn = request.form['first_name']
+        ln = request.form['last_name']
+        email = request.form['email']
+        new_user = User(first_name=fn, last_name=ln, email=email)
+        db_session.add(new_user)
+        db_session.commit()
+        return redirect(url_for('login_user'))
+    return render_template('register.html')
 
-@app.route('/about/')
-def about():
-    return render_template ('about.html')
+@app.route('/login', methods=['POST', 'GET'])
+def login_user():
+    if request.method == "POST":
+        email = request.form['email']
+        password = request.form['password'] 
+        user = db_session.query(User).filter_by(email=email).first()
+        if user:
+            session['user_id'] = user.id
+            session['user_name'] = user.first_name
+            flash(f'خوش آمدید {user.first_name}!', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash('ایمیل یا رمز عبور اشتباه است.', 'danger')
+            return redirect(url_for('login_user'))
+    return render_template('signin.html')
 
-@app.route('/blog/')
-def blog():
-    return render_template ('blog.html')
+@app.route('/logout')
+def logout():
+    session.pop('user_id', None)
+    session.pop('user_name', None)
+    flash('با موفقیت خارج شدید' , 'info')
+    return redirect(url_for('login_user'))
 
-@app.route('/index/')
+@app.route('/')
 def home():
-    return render_template ('index.html')
+    return render_template('index.html')
 
-@app.route('/register/')
-def register():
-    return render_template ('register.html')
+@app.route('/about')
+def about():
+    return render_template('about.html')
 
-@app.route('/signin/')
-def signin():
-    return render_template ('signin.html')
+@app.route('/blog')
+def blog():
+    return render_template('blog.html')
 
-@app.route('/signout/')
-def signout():
-    return render_template ('signout.html')
-
-@app.route('/service/')
-def service():
-    return render_template ('service.html')
-
-@app.route('/team/')
-def team():
-    return render_template ('team.html')
-
-@app.route('/contact/')
+@app.route('/contact')
 def contact():
-    return render_template ('contact.html')
+    return render_template('contact.html')
+
+@app.route('/error')
+def error():
+    return render_template('error.html')
+
+@app.route('/register_page')
+def register_page():
+    return render_template('register.html')
+
+@app.route('/search_result')
+def search_result():
+    return render_template('search_result.html')
+
+@app.route('/service')
+def service():
+    return render_template('service.html')
+
+@app.route('/team')
+def team():
+    return render_template('team.html')
+
+@app.route('/profile')
+def profile():
+    return render_template('profile.html')
+
 if __name__ == "__main__":
     app.run(debug=True)
-
-
-# @app.route('/details/<int:id>') 
-# def details(id):
-#     u = session.query(User).get(id)
-#     return render_template('details.html', u=u)
-
-
-# @app.route('/delete/<int:id>') 
-# def delete(id):
-#     u = session.query(User).get(id)
-#     session.delete(u)
-#     session.commit()
-#     return redirect(url_for('index'))
-
-# @app.route('/update/<int:id>', methods=['POST', 'GET'])
-# def update(id):
-#     us = session.query(User).get(id)
-#     if request.method == "POST":
-#         fn = request.form['first_name']
-#         ln = request.form['last_name']
-#         e = request.form['email']
-#         us.first_name = fn
-#         us.last_name = ln
-#         us.email = e
-#         session.commit()
-#         return redirect(url_for('index'))
-#     return render_template('update.html', us=us)
-
-
-# @app.route('/create', methods=['POST', 'GET']) 
-# def create():
-#     if request.method=="POST":
-#         fn = request.form['first_name']
-#         ln = request.form['last_name']
-#         e = request.form['email']
-#         email = request.form['email']
-#         new_user = User(first_name=fn, last_name=ln, email=email)
-#         session.add(new_user)
-#         session.commit()
-#         return redirect(url_for('index'))
-#     return render_template('create.html')
-# @app.route('/posts') 
-# def posts():
-#     return render_template('posts.html')
-
-
